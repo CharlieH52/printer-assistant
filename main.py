7ba466e681392793871b5ef900e810b45b69852e
CharlieH52 <carlos.gomez.h52@gmail.com>
2024-07-01 14:41:19 -0600

    UPDATE

--- main.py
+++ main.py
@@ -11,12 +11,9 @@ ls_clear = []
 def clean_list():
     for printer in ls_printer:
         item_pr = re.sub(r' 0.*', '', printer).strip()
-        for index in IGNORE:
-            if index in item_pr:
-                pass
-            else:
-                ls_clear.append(item_pr)
+        if all(index not in item_pr for index in IGNORE):
+            ls_clear.append(item_pr)
 
 clean_list()
 for item in ls_clear:
-    print(item)
+    print(f"{ls_clear.}. {item}")
